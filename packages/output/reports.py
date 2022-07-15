7e847a464678de534edd70f3bd2b18a3955c8b04
devon-ripley <derippet@gmail.com>
2022-07-15 22:48:00 +0000

    fixing automatic trading reports

--- packages/output/reports.py
+++ packages/output/reports.py
@@ -4,66 +4,79 @@ from packages.output import trade_sql
 from pathlib import Path
 from packages.oanda_api import oanda_api
 
+class WeeklyReport:
 
-def end_of_week(apikey, account_id):
-    date = datetime.datetime.now().date()
-    year = date.year
-    start_date = date - datetime.timedelta(days=7)
-    start_date_str = str(start_date)
-    year_str = str(year)
-    path_to_output = 'data/reports/report_' + year_str + '.csv'
-    # get all trade info from mysql for this week
-    data = trade_sql.all_complete_trades_from(start_date_str)
-    #get current balance from oanda_api
-    balance = float(oanda_api.account_summary(apikey, account_id)['account']['balance'])
-    path = Path(path_to_output)
-    headers = ['date', 'profit_week', 'trades_week', 'profit_year', 'trades_year', 'balance']
+    def __init__(self, apikey, account_id):
+        self.apikey = apikey
+        self.account_id = account_id
+        self.year = datetime.datetime.now().year
 
-    # calculate total profit for week
-    profit_week = 0.0
-    trades_week = 0
-    for x in data:
-        if x[11] == 'N/A':
-            continue
-        profit_week += float(x[11])
-        trades_week += 1
-    # calculate year profit and number of trades
-    total_profit = 0.0
-    total_trades = 0
-    if path.is_file():
-        file = open(path_to_output, 'r')
-        lines = file.readlines()
-        file.close()
-        for i, line in enumerate(lines):
-            if i == 0:
+    def find_last_week(self):
+        path = 'data/reports/report_' + str(self.year) + '.csv'
+        with open(path, 'r') as file:
+            data = csv.reader(file)
+            data = list(data)
+        last_line = data[-1]
+        # check date if before last friday
+
+    def end_of_week(self, apikey, account_id):
+        date = datetime.datetime.now().date()
+        start_date = date - datetime.timedelta(days=7)
+        start_date_str = str(start_date)
+        year_str = str(self.year)
+        path_to_output = 'data/reports/report_' + year_str + '.csv'
+        # get all trade info from mysql for this week
+        data = trade_sql.all_complete_trades_from(start_date_str)
+        #get current balance from oanda_api
+        balance = float(oanda_api.account_summary(apikey, account_id)['account']['balance'])
+        path = Path(path_to_output)
+        headers = ['date', 'profit_week', 'trades_week', 'profit_year', 'trades_year', 'balance']
+
+        # calculate total profit for week
+        profit_week = 0.0
+        trades_week = 0
+        for x in data:
+            if x[11] == 'N/A':
                 continue
-            else:
-                wk_prof = float(line[1])
-                wk_tra = int(line[2])
-                total_profit = total_profit + wk_prof
-                total_trades = total_trades + wk_tra
-        total_profit = total_profit + profit_week
-        total_trades = total_trades + trades_week
-        report_data = {
-            'date': str(date),
-            'profit_week': profit_week,
-            'trades_week': trades_week,
-            'profit_year': total_profit,
-            'trades_year': total_trades,
-            'balance': balance
-        }
-        # add data to file
-        with open(path_to_output, 'a', newline='') as f:
-            writer_object = csv.DictWriter(f, fieldnames=headers)
-            writer_object.writerow(report_data)
-            f.close()
-    else:
-        total_profit = profit_week
-        total_trades = total_trades
-        file = open(path_to_output, 'w')
-        file.writelines(['date,profit_week,trades_week,profit_year,trades_year,balance\n',
-                         f'{str(date)},{profit_week},{trades_week},{total_profit},{total_trades},{balance}\n'])
-        file.close()
+            profit_week += float(x[11])
+            trades_week += 1
+        # calculate year profit and number of trades
+        total_profit = 0.0
+        total_trades = 0
+        if path.is_file():
+            file = open(path_to_output, 'r')
+            lines = file.readlines()
+            file.close()
+            for i, line in enumerate(lines):
+                if i == 0:
+                    continue
+                else:
+                    wk_prof = float(line[1])
+                    wk_tra = int(line[2])
+                    total_profit = total_profit + wk_prof
+                    total_trades = total_trades + wk_tra
+            total_profit = total_profit + profit_week
+            total_trades = total_trades + trades_week
+            report_data = {
+                'date': str(date),
+                'profit_week': profit_week,
+                'trades_week': trades_week,
+                'profit_year': total_profit,
+                'trades_year': total_trades,
+                'balance': balance
+            }
+            # add data to file
+            with open(path_to_output, 'a', newline='') as f:
+                writer_object = csv.DictWriter(f, fieldnames=headers)
+                writer_object.writerow(report_data)
+                f.close()
+        else:
+            total_profit = profit_week
+            total_trades = total_trades
+            file = open(path_to_output, 'w')
+            file.writelines(['date,profit_week,trades_week,profit_year,trades_year,balance\n',
+                             f'{str(date)},{profit_week},{trades_week},{total_profit},{total_trades},{balance}\n'])
+            file.close()
 
 
 # not in use
